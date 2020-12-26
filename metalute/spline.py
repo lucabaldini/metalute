e62255a11ee255ab6a5e736ad8507a134b6a01dc
Luca Baldini <luca.baldini@pi.infn.it>
2020-12-26 02:08:31 +0100

    Minor.

--- metalute/spline.py
+++ metalute/spline.py
@@ -30,6 +30,8 @@ from metalute.blueprint import blueprint
 from metalute.geometry import Point
 from metalute.fret import Fretboard
 from metalute.pickup import SingleCoilRouting, HumbuckerRouting
+from metalute.bridge import HardtailBridgeBase
+
 
 
 class ParametricSpline:
@@ -53,14 +55,31 @@ class ParametricSpline:
         kwargs.setdefault('color', 'orange')
         plt.plot(self.x + offset.x, self.y + offset.y, 'x', **kwargs)
 
-    def draw(self, offset, **kwargs):
+    def draw(self, offset, tmin=0., tmax=1., num_points=1000, **kwargs):
         """
         """
         kwargs.setdefault('color', 'black')
-        t = np.linspace(0., 1., 1000)
+        t = np.linspace(tmin, tmax, num_points)
         x, y = interpolate.splev(t, self.tck)
         plt.plot(x + offset.x, y + offset.y, **kwargs)
 
+    def calculate_contour(self, offset, border=-15., tmin=0., tmax=1., num_points=1000):
+        """
+        """
+        t = np.linspace(tmin, tmax, num_points)
+        x, y = interpolate.splev(t, self.tck)
+        dx, dy = interpolate.splev(t, self.tck, 1)
+        phi = np.arctan2(dx, -dy)
+        x += border * np.cos(phi) + offset.x
+        y += border * np.sin(phi) + offset.y
+        return x, y
+
+    def draw_contour(self, offset, border=-15., tmin=0., tmax=1., num_points=1000, **kwargs):
+        """
+        """
+        kwargs.setdefault('color', 'black')
+        x, y = self.calculate_contour(offset, border, tmin, tmax, num_points)
+        plt.plot(x, y, **kwargs)
 
 
 
@@ -84,15 +103,42 @@ def test_draw():
     s.draw(offset)
     #s.draw_points(offset)
 
+    h1 = 52.
+    h2 = 25.
+    w1 = 75.
+    x, y = s.calculate_contour(offset, tmin=0., tmax=0.5)
+    mask = abs(y) > h1
+    x = x[mask]
+    y = y[mask]
+    plt.plot(x, y, color='black')
+    plt.vlines(x[0], h1, h2)
+    plt.hlines(h2, x[0], x[0] + w1)
+    plt.vlines(x[0] + w1, h2, h1)
+    plt.hlines(h1, x[0] + w1, x[-1])
+    x, y = s.calculate_contour(offset, tmin=0.5, tmax=1.)
+    mask = np.logical_and(abs(y) > h1, x - offset.x < 317.)
+    x = x[mask]
+    y = y[mask]
+    plt.plot(x, y, color='black')
+    plt.hlines(-h1, x[0], x[-1] + w1)
+    plt.vlines(x[-1] + w1, -h1, -h2)
+    plt.hlines(-h2, x[-1], x[-1] + w1)
+    plt.vlines(x[-1], -h1, -h2)
+
+
     p0 = Point(-50., 0)
     l = Line(p0, p0.move(500., 0.))
     l.draw(offset)
 
+    p1 = Point(590., 0.)
     fretboard = Fretboard(num_frets=22, reference_fret=22, width_at_nut=42.,
                           width_at_reference_fret=56., scale_length=648.)
     fretboard.draw_top_shape((590., 0.))
     fretboard.draw_top_frets((590., 0.), indices=False)
     fretboard.draw_bridge_reference((590., 0.))
+
+    bridge = HardtailBridgeBase()
+    bridge.draw(p1.hmove(-fretboard.scale_length))
 
     pickup = HumbuckerRouting()
     pickup.draw(Point(-10., 0.))
@@ -105,4 +151,5 @@ def test_draw():
 
 if __name__ == '__main__':
     test_draw()
+    plt.savefig('ej1.pdf')
     plt.show()
